140e584120d05f210943b027abf29702a9bc21a5
Muhammad Hisham <72977798+Muhammad-Hishamm@users.noreply.github.com>
2025-06-27 01:53:25 +0300

    Feat : N-Tier Architecture, Data Context Class for Hospital Project, Connection String Class, Register Connection String in Program cs file, Add Identity into the project, Add WebSite Roles and DataSeeding for Roles and Default Admin

---
+++ HospitalWebApp/Hospital.Utilities/DbInitializer.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Principal;
+using System.Text;
+using System.Threading.Tasks;
+using Hospital.Repositories;
+using Microsoft.EntityFrameworkCore;
+using Hospital.Models;
+
+namespace Hospital.Utilities
+{
+    public class DbInitializer : IDbInitializer
+    {
+        private UserManager<ApplicationUser> _userManager;
+        private RoleManager<IdentityRole> _roleManager;
+        private ApplicationDbContext _context;
+
+        public DbInitializer(UserManager<ApplicationUser> userManager, RoleManager<IdentityRole> roleManager, ApplicationDbContext context)
+        {
+            _userManager = userManager;
+            _roleManager = roleManager;
+            _context = context;
+        }
+        /// <summary>
+
+        public void Initialize()
+        {
+            try
+            {
+                if(_context.Database.GetPendingMigrations().Count() > 0)
+                {
+                    _context.Database.Migrate();
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Database migration failed: " + ex.Message);
+            }
+
+            if (!_roleManager.RoleExistsAsync(WebSiteRoles.WebSite_Admin).GetAwaiter().GetResult())
+            {
+                _roleManager.CreateAsync(new IdentityRole(WebSiteRoles.WebSite_Admin)).GetAwaiter().GetResult();
+                _roleManager.CreateAsync(new IdentityRole(WebSiteRoles.WebSite_Paient)).GetAwaiter().GetResult();
+                _roleManager.CreateAsync(new IdentityRole(WebSiteRoles.WebSite_Doctor)).GetAwaiter().GetResult();
+
+                _userManager.CreateAsync(new ApplicationUser{ UserName = "TEKA",Email="teka@gmail.com"},"teka@123").GetAwaiter().GetResult();
+                var  Appuser = _context.ApplicationUsers.FirstOrDefault(AppUser => AppUser.Email == "teka@gmail.com");
+                if(Appuser != null)
+                {
+                   _userManager.AddToRoleAsync(Appuser, WebSiteRoles.WebSite_Admin).GetAwaiter().GetResult();
+                }
+
+            }
+
+        }
+    }
+}
